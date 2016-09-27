30fb18e448f5b9ffc93300f7b0a0d32ecc36275a
Juan Carlos Prieto <juanprietob@gmail.com>
2016-09-27 11:36:34 -0400

    BUG: Remove addition of executables. It is handled by macro SEMMacroBuild

--- SPHARM-PDM.cmake
+++ SPHARM-PDM.cmake
@@ -13,18 +13,8 @@ endif()
 include(${CMAKE_CURRENT_SOURCE_DIR}/Common.cmake)
 #-----------------------------------------------------------------------------
 
-find_package(SlicerExecutionModel REQUIRED GenerateCLP)
-include(${GenerateCLP_USE_FILE})
-include(${SlicerExecutionModel_USE_FILE})
-include(${SlicerExecutionModel_CMAKE_DIR}/SEMMacroBuildCLI.cmake)
-#-----------------------------------------------------------------------------
-
-find_package(BatchMake REQUIRED)
-include(${BatchMake_USE_FILE})
-#-----------------------------------------------------------------------------
-
 set(expected_ITK_VERSION_MAJOR ${ITK_VERSION_MAJOR})
-find_package(ITK NO_MODULE REQUIRED)
+find_package(ITK REQUIRED)
 if(${ITK_VERSION_MAJOR} VERSION_LESS ${expected_ITK_VERSION_MAJOR})
   # Note: Since ITKv3 doesn't include a ITKConfigVersion.cmake file, let's check the version
   #       explicitly instead of passing the version as an argument to find_package() command.
@@ -34,6 +24,17 @@ if(${ITK_VERSION_MAJOR} VERSION_LESS ${expected_ITK_VERSION_MAJOR})
                       "  ${ITK_CONFIG}, version: ${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_PATCH}\n")
 endif()
 include(${ITK_USE_FILE})
+
+
+find_package(SlicerExecutionModel REQUIRED)
+#include(${GenerateCLP_USE_FILE})
+include(${SlicerExecutionModel_USE_FILE})
+#include(${SlicerExecutionModel_CMAKE_DIR}/SEMMacroBuildCLI.cmake)
+#-----------------------------------------------------------------------------
+message(STATUS ddd${ITK_DIR})
+find_package(BatchMake REQUIRED)
+include(${BatchMake_USE_FILE})
+#-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
 find_package(VTK REQUIRED)
@@ -82,33 +83,3 @@ IF(BUILD_TESTING)
   include(CTest)
   ADD_SUBDIRECTORY(Testing)
 ENDIF(BUILD_TESTING)
-
-
-if( SPHARM-PDM_BUILD_SLICER_EXTENSION )
-  unsetForSlicer( NAMES VTK_DIR SlicerExecutionModel_DIR DCMTK_DIR ITK_DIR CMAKE_C_COMPILER CMAKE_CXX_COMPILER CMAKE_CXX_FLAGS CMAKE_C_FLAGS )
-  # Create fake imported target to avoid importing Slicer target: See SlicerConfig.cmake:line 820
-  add_library(SlicerBaseLogic SHARED IMPORTED)
-  find_package(Slicer REQUIRED)
-  include(${Slicer_USE_FILE})
-  set( EXTENSION_NO_CLI asc2meta asc2vtk BYU2VTK MeshMath Meta2STL Meta2VTK SpharmTool STL2Meta VTK2Meta )
-  set( EXTENSION_CLI GenParaMeshCLP ParaToSPHARMMeshCLP RadiusToMesh SegPostProcessCLP ShapeAnalysisModule  )
-  set( EXTENSION_TESTS ${EXTENSION_NO_CLI} ${EXTENSION_CLI} GenParaMeshCLPTest SegPostProcessCLPTest )
-  foreach( VAR ${EXTENSION_TESTS})
-    add_executable(${VAR} IMPORTED)
-    set_property(TARGET ${VAR} PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/${proj}-install/${${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION}/${VAR}${fileextension})
-  endforeach()
-  if(BUILD_TESTING)
-    add_subdirectory(Testing)
-  endif()
-  resetForSlicer( NAMES VTK_DIR ITK_DIR SlicerExecutionModel_DIR CMAKE_C_COMPILER CMAKE_CXX_COMPILER CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
-  set( NOCLI_INSTALL_DIR ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION}/../ExternalBin)
-  foreach( VAR ${EXTENSION_NO_CLI})
-    install( PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${proj}-install/${${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION}/${VAR}${fileextension} DESTINATION ${NOCLI_INSTALL_DIR} )
-  endforeach()
-  foreach( VAR ${EXTENSION_CLI})
-    install( PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${proj}-install/${${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION}/${VAR}${fileextension} DESTINATION ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION} )
-  endforeach()
-  install( DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/bmm DESTINATION ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION}/.. )
-  set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CMAKE_BINARY_DIR};${EXTENSION_NAME};ALL;/")
-  include(${Slicer_EXTENSION_CPACK})
-endif()
